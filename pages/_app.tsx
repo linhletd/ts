6e1ab3ee0891729fe8ad52aa9f6c6094c34311ba
linhltd <linhletd@gmail.com>
2021-05-25 10:02:15 +0700

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import 'src/util/i18n';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
